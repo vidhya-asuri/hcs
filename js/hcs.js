491495f1f52b8d6bcc03a4db4ca0ea02772b1573
Vidhya <sas227@uky.edu>
2020-03-17 14:30:30 -0400

    try out chartjs

--- js/hcs.js
+++ js/hcs.js
@@ -20,6 +20,8 @@ $( document ).ready(function() {
 	    		d3.json("county.json",function(err,data) {
 	    			  displayChart(data);
 	    			  // update(data1,transition=false);
+	    			  
+	    			  
 	    		});
 	    		
 	    		var data = xhr.response;
@@ -36,6 +38,44 @@ $( document ).ready(function() {
 	  document.getElementById('quarterly').onclick = displayQuarterlyData;
 	  document.getElementById('yearly').onclick = displayYearlyData;
 
+	  
+	  var ctx = document.getElementById('myChart').getContext('2d');
+	  var myChart = new Chart(ctx, {
+	      type: 'bar',
+	      data: {
+	          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+	          datasets: [{
+	              label: '# of Votes',
+	              data: [12, 19, 3, 5, 2, 3],
+	              backgroundColor: [], /*[ 
+	                  'rgba(255, 99, 132, 0.2)',
+	                  'rgba(54, 162, 235, 0.2)',
+	                  'rgba(255, 206, 86, 0.2)',
+	                  'rgba(75, 192, 192, 0.2)',
+	                  'rgba(153, 102, 255, 0.2)',
+	                  'rgba(255, 159, 64, 0.2)'
+	              ],*/
+	              borderColor: [
+	                  'rgba(255, 99, 132, 1)',
+	                  'rgba(54, 162, 235, 1)',
+	                  'rgba(255, 206, 86, 1)',
+	                  'rgba(75, 192, 192, 1)',
+	                  'rgba(153, 102, 255, 1)',
+	                  'rgba(255, 159, 64, 1)'
+	              ],
+	              borderWidth: 1
+	          }]
+	      },
+	      options: {
+	          scales: {
+	              yAxes: [{
+	                  ticks: {
+	                      beginAtZero: true
+	                  }
+	              }]
+	          }
+	      }
+	  });
 	  
 	  
 });
